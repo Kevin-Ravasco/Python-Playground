b766363612c1e7e049458078d8603613be2e598f
Kevin O <85694323+Kevin-Ravasco2@users.noreply.github.com>
2022-06-16 20:05:24 +0300

    try block traversal algorithm

---
+++ Algorithms/block_traversal.py
@@ -0,0 +1,38 @@
+blocks = [
+    {
+        "gym": False,
+        "school": True,
+        "store": False
+    },
+    {
+        "gym": True,
+        "school": False,
+        "store": False
+    },
+    {
+        "gym": True,
+        "school": True,
+        "store": False
+    },
+    {
+        "gym": False,
+        "school": True,
+        "store": False
+    },
+    {
+        "gym": False,
+        "school": True,
+        "store": True
+    },
+]
+
+requirements = ["gym", "school", "store"]
+
+# binary_block = [[0, 1, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 1, 1]]
+
+
+def shortest_distance_block(requirements):
+    for req in requirements:
+        for block in blocks:
+            pass
+    pass
